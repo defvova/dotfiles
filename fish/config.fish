e8dc29323e7fd6cac5a6cfe601a6b0b3e238dc9b
Volodymyr Partytskyi <volodymyr.partytskyi@diymaxeda.com>
2023-01-02 20:56:06 +0200

    Adjust fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ set -U fish_greeting
 
 alias spotify='ncspot'
 alias lg='lazygit'
+alias ld='lazydocker'
 alias vim='nvim'
 alias ls='lsd -A'
 alias ll='lsd -lA'
@@ -18,21 +19,10 @@ alias docker='podman'
 alias docker-compose='podman-compose'
 alias reload='exec fish'
 
-function npm-login
-  if grep -q "codeartifact" ~/.npmrc
-    aws codeartifact login --tool npm --domain maxeda --repository npm
-  else
-    echo "You are currently using Artifactory, login is not needed!"
-  end
-end
-
-function build_fabric
-  set -l current_path $PWD
-  rm -rf ~/work/bitbucket/frontend/fabricsjs/packages/fabricsjs/dist && ~/work/bitbucket/frontend/fabricsjs/packages/fabricsjs & npm run build && rm -rf ~/work/bitbucket/frontend/buying-advice/node_modules/@maxeda/fabricsjs/dist && rm -rf ~/work/bitbucket/frontend/buying-advice/node_modules/@maxeda/fabricsjs/src && cp -R ~/work/bitbucket/frontend/fabricsjs/packages/fabricsjs/dist ~/work/bitbucket/frontend/buying-advice/node_modules/@maxeda/fabricsjs && cp -R ~/work/bitbucket/frontend/fabricsjs/packages/fabricsjs/src ~/work/bitbucket/frontend/buying-advice/node_modules/@maxeda/fabricsjs
-  cd $current_path
-end
-
+starship init fish | source
 frum init | source
 fnm env --use-on-cd | source
-starship init fish | source
 zoxide init fish | source
+
+fish_add_path -g ~/.cargo/bin
+# test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
