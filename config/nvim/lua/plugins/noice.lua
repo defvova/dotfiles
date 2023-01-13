ed610e1968b83d4aa1447769ef57d1ef246b0a66
Volodymyr Partytskyi <volodymyr.partytskyi@diymaxeda.com>
2023-01-13 20:16:24 +0200

    Adjust noice lsp config

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -16,6 +16,13 @@ function M.config()
     messages = {
       enabled = false,
     },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
   }
 
   noice.setup(options)
