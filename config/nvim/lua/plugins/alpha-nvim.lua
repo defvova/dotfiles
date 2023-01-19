a52d32e2aad7692024da23b1d0b2fb271f8442c9
Volodymyr Partytskyi <volodymyr.partytskyi@diymaxeda.com>
2023-01-19 09:42:45 +0200

    Fix rendering info in dashboard footer

--- config/nvim/lua/plugins/alpha-nvim.lua
+++ config/nvim/lua/plugins/alpha-nvim.lua
@@ -52,10 +52,11 @@ function M.config()
     -- Number of plugins
     local datetime = os.date "%d-%m-%Y %H:%M:%S"
     local stats = require("lazy").stats()
-    local plugins_text = "   "
+    local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+    local plugins_text = " ⚡Neovim loaded "
         .. stats.count
         .. " plugins in "
-        .. (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        .. ms
         .. "ms"
         .. "   v"
         .. vim.version().major
@@ -73,13 +74,19 @@ function M.config()
     return plugins_text .. "\n" .. quote
   end
 
-  dashboard.section.footer.val = footer()
-
   dashboard.section.footer.opts.hl = "Type"
   dashboard.section.header.opts.hl = "Include"
   dashboard.section.buttons.opts.hl = "Keyword"
 
   alpha.setup(dashboard.opts)
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyVimStarted",
+    callback = function()
+      dashboard.section.footer.val = footer()
+      pcall(vim.cmd.AlphaRedraw)
+    end,
+  })
 end
 
 return M
