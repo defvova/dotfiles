c01e039af3eb1d18776adba39812c76be48da9c2
Volodymyr Partytskyi <volodymyr.partytskyi@diymaxeda.com>
2023-01-31 00:16:10 +0200

    Update telescope options

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
   dependencies = {
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     {
       "nvim-telescope/telescope-project.nvim",
       dependencies = { "nvim-telescope/telescope-file-browser.nvim" },
@@ -21,70 +22,54 @@ return {
     { "<leader>sC", "<cmd> Telescope commands <CR>", "   show commands", mode = "v" },
   },
   opts = {
-    defaults = {
-      vimgrep_arguments = {
-        "rg",
-        "-L",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
+    extensions = {
+      fzf = {
+        fuzzy = false,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
       },
+    },
+    defaults = {
       prompt_prefix = "   ",
       selection_caret = "  ",
-      entry_prefix = "  ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
       sorting_strategy = "ascending",
-      layout_strategy = "horizontal",
       layout_config = {
         horizontal = {
           prompt_position = "top",
-          preview_width = 0.55,
-          results_width = 0.8,
         },
-        vertical = {
-          mirror = false,
-        },
-        width = 0.87,
-        height = 0.80,
-        preview_cutoff = 120,
       },
-      -- file_sorter = require("telescope.sorters").get_fuzzy_file,
-      file_ignore_patterns = { "node_modules" },
-      -- generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+      file_ignore_patterns = { "node_modules", ".git" },
       path_display = { "truncate" },
-      winblend = 0,
       border = {},
       borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-      color_devicons = true,
-      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-      -- file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      -- grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      -- qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-      -- buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-      -- mappings = {
-      --   n = { ["q"] = require("telescope.actions").close },
-      -- },
-    },
-
-    extensions_list = {
-      "file_browser",
-      "project",
-      "themes",
-      "terms",
+      set_env = { ["COLORTERM"] = "truecolor" },
+      mappings = {},
     },
   },
   config = function(_, opts)
+    local extensions_list = {
+      "file_browser",
+      "project",
+      "fzf",
+      "noice",
+    }
     local telescope = require "telescope"
+    local actions = require "telescope.actions"
+    local previewers = require "telescope.previewers"
+    local sorters = require "telescope.sorters"
+
+    opts.defaults.file_previewer = previewers.vim_buffer_cat.new
+    opts.defaults.grep_previewer = previewers.vim_buffer_vimgrep.new
+    opts.defaults.qflist_previewer = previewers.vim_buffer_qflist.new
+    opts.defaults.buffer_previewer_maker = previewers.buffer_previewer_maker
+    opts.defaults.file_sorter = sorters.get_fuzzy_file
+    opts.defaults.generic_sorter = sorters.get_generic_fuzzy_sorter
+    opts.defaults.mappings.n = { ["q"] = actions.close }
     telescope.setup(opts)
-    -- load extensions
-    pcall(function()
-      for _, ext in ipairs(opts.extensions_list) do
-        telescope.load_extension(ext)
-      end
-    end)
+
+    for _, ext in ipairs(extensions_list) do
+      telescope.load_extension(ext)
+    end
   end,
 }
