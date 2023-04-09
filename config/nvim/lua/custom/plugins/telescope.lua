cb82c6e5eac59ec1ed534cbd6db0940715995f67
Volodymyr Partytskyi <volodymyr.partytskyi@diymaxeda.com>
2023-04-09 23:35:27 +0300

    Adjust telescope settings

--- config/nvim/lua/custom/plugins/telescope.lua
+++ config/nvim/lua/custom/plugins/telescope.lua
@@ -31,57 +31,72 @@ return {
     { "<leader>sd", "<cmd>Telescope diagnostics<CR>", desc = "[S]earch [D]iagnostics" },
     { "<leader>sp", "<cmd>Telescope workspaces<CR>", desc = "[S]earch [P]rojects" },
   },
-  opts = {
-    defaults = {
-      prompt_prefix = "   ",
-      selection_caret = "  ",
-      sorting_strategy = "ascending",
-      layout_config = {
-        horizontal = {
-          prompt_position = "top",
-        },
-      },
-      path_display = { "truncate" },
-      border = {},
-      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-      set_env = { ["COLORTERM"] = "truecolor" },
-      mappings = {
-        i = {
-          ["<C-u>"] = false,
-          ["<C-d>"] = false,
-        },
-      },
-    },
-    pickers = {
-      find_files = {
-        results_title = false,
-        previewer = false,
+  opts = function()
+    local actions = require "telescope.actions"
+    local layout_actions = require "telescope.actions.layout"
+
+    return {
+      defaults = {
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        dynamic_preview_title = true,
+        sorting_strategy = "ascending",
+        cycle_layout_list = { "flex", "horizontal", "vertical", "bottom_pane", "center" },
         layout_config = {
-          width = 80,
+          horizontal = {
+            prompt_position = "top",
+          },
+        },
+        history = { path = vim.fn.stdpath "data" .. "/telescope_history.sqlite3" },
+        layout_strategy = "flex",
+        path_display = { "truncate" },
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        set_env = { ["COLORTERM"] = "truecolor" },
+        mappings = {
+          i = {
+            ["<C-s>"] = actions.select_horizontal,
+            ["<C-j>"] = actions.cycle_history_next,
+            ["<C-k>"] = actions.cycle_history_prev,
+            ["<C-e>"] = layout_actions.toggle_preview,
+            ["<C-l>"] = layout_actions.cycle_layout_next,
+            ["<C-/>"] = actions.which_key,
+            ["<C-r>"] = actions.to_fuzzy_refine,
+            ["<Tab>"] = actions.toggle_selection,
+          },
         },
       },
-      oldfiles = {
-        results_title = false,
-        previewer = false,
-        layout_config = {
-          width = 80,
+      pickers = {
+        find_files = {
+          -- results_title = false,
+          previewer = false,
+          layout_config = {
+            width = 80,
+          },
+        },
+        oldfiles = {
+          -- results_title = false,
+          previewer = false,
+          layout_config = {
+            width = 80,
+          },
+        },
+        live_grep = {
+          theme = "dropdown",
+        },
+        grep_string = {
+          theme = "dropdown",
+        },
+        buffers = {
+          theme = "dropdown",
         },
       },
-      live_grep = {
-        theme = "dropdown",
+      extensions = {
+        fzf = {},
+        luasnip = {},
       },
-      grep_string = {
-        theme = "dropdown",
-      },
-      buffers = {
-        theme = "dropdown",
-      },
-    },
-    extensions = {
-      fzf = {},
-      luasnip = {},
-    },
-  },
+    }
+  end,
   config = function(_, opts)
     local extensions_list = {
       "fzf",
