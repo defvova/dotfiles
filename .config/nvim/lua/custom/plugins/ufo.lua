6212f4eb9d6cb9439cb52f0509f0a1f2e8985ac5
Vova <volodimir.partytskyi@gmail.com>
2024-03-14 21:23:28 +0200

    Fix deprecation option for ufo plugin

--- .config/nvim/lua/custom/plugins/ufo.lua
+++ .config/nvim/lua/custom/plugins/ufo.lua
@@ -9,10 +9,9 @@ return {
     --   "zR", "zM"
     -- },
     config = function()
-      --- @diagnostic disable: unused-local
       require("ufo").setup({
         open_fold_hl_timeout = 150,
-        close_fold_kinds = { 'imports', 'comment' },
+        close_fold_kinds_for_ft = { default = { 'imports', 'comment' } }
         -- provider_selector = function(_bufnr, _filetype, _buftype)
         --   return { "treesitter", "indent" }
         -- end,
