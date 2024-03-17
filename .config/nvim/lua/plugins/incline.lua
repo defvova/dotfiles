80bb37a1e43e6c9bdd7eccb3244222d89dc8399d
Vova <volodimir.partytskyi@gmail.com>
2024-03-17 17:06:25 +0200

    Add incline plugin

---
+++ .config/nvim/lua/plugins/incline.lua
@@ -0,0 +1,53 @@
+return {
+  'b0o/incline.nvim',
+  event = 'VeryLazy',
+  config = function()
+    local helpers = require 'incline.helpers'
+    local devicons = require 'nvim-web-devicons'
+    require('incline').setup {
+      window = {
+        zindex = 500,
+        padding = 0,
+        padding_char = " ",
+        margin = {
+          horizontal = 1,
+          vertical = 0,
+        },
+        -- overlap = {
+        --   winbar = true,
+        --   tabline = false,
+        -- },
+      },
+      highlight = {
+        groups = {
+          InclineNormal = {
+            default = true,
+            group = "TabLine",
+          },
+          InclineNormalNC = {
+            default = true,
+            group = "TabLine",
+          },
+        },
+      },
+      debounce_threshold = {
+        rising = 10,
+        falling = 10,
+      },
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+        if filename == '' then
+          filename = '[No Name]'
+        end
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+
+        return {
+          ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+          ' ',
+          { filename, gui = modified and 'bold,italic' or 'bold' }
+        }
+      end,
+    }
+  end
+}
