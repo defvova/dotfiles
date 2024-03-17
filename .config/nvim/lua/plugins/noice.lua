f496537782a188280c88b634fc584e0521925b5f
Vova <volodimir.partytskyi@gmail.com>
2024-03-17 23:27:27 +0200

    Update position for noice cmd

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -12,6 +12,98 @@ return {
         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
       },
     },
+    views = {
+      split = {
+        win_options = {
+          winhighlight = "Normal:Normal",
+        },
+      },
+      mini = {
+        win_options = {
+          winblend = 0,
+        },
+      },
+      popup = {
+        border = {
+          padding = { 0, 0 },
+        },
+        win_options = {
+          winhighlight = {
+            Normal = "NormalFloat",
+            FloatBorder = "NormalFloatInv",
+          },
+        },
+      },
+      cmdline_popup = {
+        position = {
+          row = "35%",
+          col = "50%",
+        },
+        border = {
+          padding = { 0, 0 },
+        },
+        win_options = {
+          -- winblend = 100,
+          winhighlight = {
+            Normal = "NormalFloat",
+            FloatBorder = "NormalBorder",
+          },
+          cursorline = false,
+        },
+        size = {
+          width = "auto",
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = "40%",
+          col = "50%",
+        },
+        size = {
+          width = 79,
+          height = 10,
+        },
+        border = {
+          padding = { 0, 0 },
+        },
+        win_options = {
+          winhighlight = {
+            Normal = "NormalFloat",
+            FloatBorder = "NormalFloatInv",
+          },
+        },
+      },
+    },
+    messages = {
+      enabled = true,            -- enables the Noice messages UI
+      view = "notify",           -- default view for messages
+      view_error = "notify",     -- view for errors
+      view_warn = "notify",      -- view for warnings
+      view_history = "messages", -- view for :messages
+      view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+    },
+    notify = {
+      enabled = true,
+    },
+    routes = {
+      {
+        filter = {
+          any = {
+            -- this filters messages to be shown by fidget
+            { find = "^[^%s]+%s(%d+)L, (%d+)B%s+%[w%]$" },
+          },
+        },
+        opts = {
+          stop = true,
+          skip = true, -- change to false to show
+          format = {
+            "{message}",
+          },
+        },
+      },
+    },
     presets = {
       bottom_search = false,        -- use a classic bottom cmdline for search
       command_palette = true,       -- position the cmdline and popupmenu together
